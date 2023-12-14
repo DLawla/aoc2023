7814b49ec77447b7e39de1ab876641bc87ca8d7b
Dan Lawler <dan.r.lawler@gmail.com>
2023-12-13 20:30:49 -0500

    templating 8 and 9

---
+++ 9/1.rb
@@ -0,0 +1,11 @@
+file = File.open("9/test.txt")
+contents = file.read
+
+lines = contents.split("\n")
+
+# stuff = lines.map do |line|
+#   _ = line.chars.select { |char| char =~ /\d/ }
+#   ...
+# end
+
+# puts all_numbers.sum
