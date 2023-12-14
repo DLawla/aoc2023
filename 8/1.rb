09bafb5d64a343dbe351a372485e69934e476016
Dan Lawler <dan.r.lawler@gmail.com>
2023-12-13 22:31:37 -0500

    day 8, 1

--- 8/1.rb
+++ 8/1.rb
@@ -1,11 +1,48 @@
-file = File.open("8/test.txt")
+require "ostruct"
+
+file = File.open("8/input.txt")
 contents = file.read
 
 lines = contents.split("\n")
 
-# stuff = lines.map do |line|
-#   _ = line.chars.select { |char| char =~ /\d/ }
-#   ...
-# end
+def new_point(location:, left:, right:)
+  OpenStruct.new(location: location, left: left, right: right)
+end
 
-# puts all_numbers.sum
+instructions = lines[0]
+points_lines = lines[2..]
+
+@points = []
+points_lines.each do |point_line|
+  coordinates = point_line.split(" = ").last
+  coordinates.gsub!("(", "")
+  coordinates.gsub!(")", "")
+  left, right = coordinates.split(", ")
+  @points << new_point(location: point_line.split(" = ").first, left:, right:)
+end
+
+location = "AAA"
+steps = 0
+
+while true do
+  instructions.chars.each do |instruction|
+    point = @points.find { |p| p.location == location }
+    if instruction == "L"
+      location = point.left
+    elsif instruction == "R"
+      location = point.right
+    end
+
+    steps += 1
+
+    if location == "ZZZ"
+      break
+    end
+  end
+
+  if location == "ZZZ"
+    break
+  end
+end
+
+puts steps
--- 9/1.rb
+++ 9/1.rb
@@ -1,11 +1,48 @@
+require "active_support/all"
+
 file = File.open("9/test.txt")
 contents = file.read
 
 lines = contents.split("\n")
 
-# stuff = lines.map do |line|
-#   _ = line.chars.select { |char| char =~ /\d/ }
-#   ...
-# end
+histories = []
 
-# puts all_numbers.sum
+lines.each do |line|
+  histories << line.scan(/\d+/).map(&:to_i)
+end
+
+predicted_values = histories.map do |history|
+  derivatives = [history]
+
+  while true do
+    last_derivative = derivatives.last
+    new_derivative = []
+    (last_derivative.length - 1).times do |i|
+      new_derivative << last_derivative[i + 1] - last_derivative[i]
+    end
+    derivatives << new_derivative
+    if new_derivative.uniq.count == 1 && new_derivative[0] == 0
+      break
+    end
+  end
+
+  # work backwards
+  reversed_derivatives = derivatives.reverse
+  reversed_derivatives.each.with_index do |derivative, i|
+    if i == 0
+      derivative << 0
+    end
+
+    if i == derivatives.length - 1
+      break
+    end
+
+    next_derivative = reversed_derivatives[i + 1]
+    next_derivative << derivative.last + next_derivative.last
+  end
+  derivatives.first.last
+end
+
+puts predicted_values
+puts "sum:"
+puts predicted_values.sum
