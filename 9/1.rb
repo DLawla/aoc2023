7f19cb597a1b75a29f23fb14088b5eb1c5301da1
Dan Lawler <dan.r.lawler@gmail.com>
2023-12-13 22:49:08 -0500

    day 9, 1

--- 9/1.rb
+++ 9/1.rb
@@ -1,6 +1,6 @@
 require "active_support/all"
 
-file = File.open("9/test.txt")
+file = File.open("9/input.txt")
 contents = file.read
 
 lines = contents.split("\n")
@@ -8,21 +8,24 @@ lines = contents.split("\n")
 histories = []
 
 lines.each do |line|
-  histories << line.scan(/\d+/).map(&:to_i)
+  # histories << line.scan(/\d+/).map(&:to_i)
+  histories << line.split(" ").map(&:to_i)
 end
 
-predicted_values = histories.map do |history|
+predicted_values = histories.map.with_index do |history,i|
   derivatives = [history]
-
   while true do
     last_derivative = derivatives.last
     new_derivative = []
     (last_derivative.length - 1).times do |i|
-      new_derivative << last_derivative[i + 1] - last_derivative[i]
+      new_derivative << (last_derivative[i + 1] - last_derivative[i])
     end
     derivatives << new_derivative
     if new_derivative.uniq.count == 1 && new_derivative[0] == 0
       break
+    end
+    if new_derivative.length == 0
+      raise "Didn't find a pattern!, history index: #{i}"
     end
   end
 
@@ -43,6 +46,5 @@ predicted_values = histories.map do |history|
   derivatives.first.last
 end
 
-puts predicted_values
 puts "sum:"
 puts predicted_values.sum
